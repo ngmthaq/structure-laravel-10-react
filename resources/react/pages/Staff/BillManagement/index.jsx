8c8624a5e476744d78e5fd20e6cee3e39ce8ea56
ngmthaq-bglobalcorp <thangnm@bglobalcorp.com>
2023-10-21 12:11:07 +0700

    Init bills

--- resources/react/pages/Staff/BillManagement/index.jsx
+++ resources/react/pages/Staff/BillManagement/index.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { Avatar, Box, Card, CardContent, CardHeader, Grid } from "@mui/material";
 import { AdminLayout } from "../../../layouts/AdminLayout";
 
 export const BillManagement = () => {
-  return <AdminLayout>BillManagement</AdminLayout>;
+  return (
+    <AdminLayout>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <Card elevation={3}>
+            <CardHeader avatar={<Avatar>N</Avatar>} title="Nguyễn Mạnh Thắng" subheader="20/10/20213 11:00 AM" />
+            <CardContent></CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </AdminLayout>
+  );
 };
